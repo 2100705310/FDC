b804c711ce208fc91470ca195ef532845ed9b52a
Engineer Brendah <107669764+2100705310@users.noreply.github.com>
2022-10-06 23:12:09 +0300

    Add files via upload

---
+++ JOB/Num.js
@@ -0,0 +1,12 @@
+Email.Send(
+    {
+        host :"akotinobrendah@gmail.com",
+        Username :"Engineer Brendah",
+        To :"namugayarose3@gmail.com",
+        From :"muyamamonica@gmail.com",
+        Subject :"This is the subject",
+        Body :"And this is the body"
+    }
+).then(
+    message => alert(message)
+);
